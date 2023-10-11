fbea810d784515a70153119026cf7ef4fc1911c8
Masoud Golestaneh <52936110+Cypherspark@users.noreply.github.com>
2023-10-11 18:15:03 +0330

    add uber automaxproc (#21)

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ import (
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
-
+	_ "go.uber.org/automaxprocs"
 	"google.golang.org/grpc"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/rest"

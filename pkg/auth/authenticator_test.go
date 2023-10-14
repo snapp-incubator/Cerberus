688b7e51ec59a02935016ec0f54a41c241e64999
ZeynabRezaei <72091463+ZeynabRezaei@users.noreply.github.com>
2023-10-15 01:08:50 +0330

    add updatecache test

--- pkg/auth/authenticator_test.go
+++ pkg/auth/authenticator_test.go
@@ -2,7 +2,17 @@ package auth
 
 import (
 	"fmt"
+	cerberusv1alpha1 "github.com/snapp-incubator/Cerberus/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"testing"
+
+	"context"
+	"github.com/stretchr/testify/assert"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
 var (
@@ -10,6 +20,10 @@ var (
 	domainAllowList = generateDomainAllowList(1000) // Generate a large domain allow list
 	testIP          = "192.168.0.1"                 // Use a valid IP for testing
 	testDomain      = "example.com"                 // Use a valid domain for testing
+	tokenSecretRef  = generateTokenSecretRef()
+	subjects        = generateSubjects(2)    // Generates ["subject-1", "subject-2"]
+	webservices     = generateWebservices(2) // Generates ["webservice-1", "webservice-2"]
+
 )
 
 func generateIPAllowList(size int) []string {
@@ -29,6 +43,31 @@ func generateDomainAllowList(size int) []string {
 
 	}
 	return domainList
+}
+
+func generateTokenSecretRef() *corev1.LocalObjectReference {
+	example := &corev1.LocalObjectReference{Name: "expmle"}
+	return example
+}
+
+func generateSubjects(subjectCount int) []string {
+	subjects := make([]string, subjectCount)
+
+	for i := 0; i < subjectCount; i++ {
+		subjects[i] = fmt.Sprintf("subject-%d", i+1)
+	}
+
+	return subjects
+}
+
+func generateWebservices(webserviceCount int) []string {
+	webservices := make([]string, webserviceCount)
+
+	for i := 0; i < webserviceCount; i++ {
+		webservices[i] = fmt.Sprintf("webservice-%d", i+1)
+	}
+
+	return webservices
 }
 
 func BenchmarkCheckIPWithLargeInput(b *testing.B) {
@@ -94,4 +133,160 @@ func TestCheckDomainComplex(t *testing.T) {
 			t.Errorf("Domain: %s, Expected Error: nil, Got Error: %v", tc.domain, err)
 		}
 	}
+}
+
+func TestUpdateCache(t *testing.T) {
+	fakeClient, authenticator := setupTestEnvironment(t)
+
+	accessTokens := prepareAccessTokens(2)
+	bindings := prepareWebserviceAccessBindings(2)
+	webservices := prepareWebservices(2)
+	secrets := prepareSecrets(2)
+
+	createAccessTokens(t, fakeClient, accessTokens[0], accessTokens[1])
+	createBindings(t, fakeClient, bindings[0], bindings[1])
+	createWebservices(t, fakeClient, webservices[0], webservices[1])
+	createSecrets(t, fakeClient, secrets[0], secrets[1])
+
+	err := authenticator.UpdateCache(fakeClient, context.Background(), false)
+
+	assert.NoError(t, err)
+
+	assertCachesPopulated(t, authenticator)
+}
+
+func setupTestEnvironment(t *testing.T) (client.Client, *Authenticator) {
+	// Initialize a Kubernetes client's scheme.
+	scheme := runtime.NewScheme()
+
+	// Register the custom resource type with the scheme.
+	err := cerberusv1alpha1.AddToScheme(scheme)
+	if err != nil {
+		t.Fatalf("Failed to add cerberusv1alpha1 to scheme: %v", err)
+	}
+
+	err = v1.AddToScheme(scheme)
+	if err != nil {
+		t.Fatalf("Failed to add v1 to scheme: %v", err)
+	}
+
+	// Create a fake Kubernetes client using the registered scheme.
+	fakeClient := fake.NewClientBuilder().WithScheme(scheme).Build()
+
+	// Create an Authenticator instance.
+	authenticator := &Authenticator{}
+
+	return fakeClient, authenticator
+}
+
+func prepareAccessTokens(count int) []cerberusv1alpha1.AccessToken {
+	// Create and prepare access tokens with unique names.
+	accessTokens := make([]cerberusv1alpha1.AccessToken, count)
+	for i := 0; i < count; i++ {
+		tokenName := fmt.Sprintf("test-token-%d", i)
+		accessTokens[i] = cerberusv1alpha1.AccessToken{
+			ObjectMeta: metav1.ObjectMeta{Name: tokenName, Namespace: "default"},
+			Spec: cerberusv1alpha1.AccessTokenSpec{
+				State:           "",
+				IpAllowList:     ipAllowList,
+				DomainAllowList: domainAllowList,
+				TokenSecretRef:  tokenSecretRef,
+			},
+		}
+	}
+	return accessTokens
+}
+
+func prepareWebserviceAccessBindings(count int) []cerberusv1alpha1.WebserviceAccessBinding {
+	// Create and prepare webservice access bindings with unique names.
+	bindings := make([]cerberusv1alpha1.WebserviceAccessBinding, count)
+	for i := 0; i < count; i++ {
+		bindingName := fmt.Sprintf("test-binding-%d", i)
+		bindings[i] = cerberusv1alpha1.WebserviceAccessBinding{
+			ObjectMeta: metav1.ObjectMeta{Name: bindingName, Namespace: "default"},
+			Spec: cerberusv1alpha1.WebserviceAccessBindingSpec{
+				Subjects:    subjects,
+				Webservices: webservices,
+			},
+		}
+	}
+	return bindings
+}
+
+func prepareWebservices(count int) []cerberusv1alpha1.WebService {
+	// Create and prepare webservice resources with unique names.
+	webservices := make([]cerberusv1alpha1.WebService, count)
+	for i := 0; i < count; i++ {
+		webserviceName := fmt.Sprintf("test-service-%d", i)
+		webservices[i] = cerberusv1alpha1.WebService{
+			ObjectMeta: metav1.ObjectMeta{Name: webserviceName, Namespace: "default"},
+			Spec: cerberusv1alpha1.WebServiceSpec{
+				LookupHeader: "X-Cerberus-Token",
+				UpstreamHttpAuth: cerberusv1alpha1.UpstreamHttpAuthService{
+					Address:       "http://example.com/auth",
+					ReadTokenFrom: "Authorization",
+					WriteTokenTo:  "X-Cerberus-Token",
+				},
+			},
+		}
+	}
+	return webservices
+}
+
+func prepareSecrets(count int) []v1.Secret {
+	// Create and prepare secrets with unique names.
+	secrets := make([]v1.Secret, count)
+	for i := 0; i < count; i++ {
+		secretName := fmt.Sprintf("test-secret-%d", i)
+		secrets[i] = v1.Secret{
+			ObjectMeta: metav1.ObjectMeta{Name: secretName, Namespace: "default"},
+			Data: map[string][]byte{
+				"token": []byte("test-token-value"),
+			},
+		}
+	}
+	return secrets
+}
+
+//func createResources(t *testing.T, fakeClient client.Client, resources ...client.Object) {
+//	ctx := context.Background()
+//	for _, resource := range resources {
+//		assert.NoError(t, fakeClient.Create(ctx, resource))
+//	}
+//}
+
+func createAccessTokens(t *testing.T, fakeClient client.Client, accessTokens ...cerberusv1alpha1.AccessToken) {
+	ctx := context.Background()
+	for _, token := range accessTokens {
+		assert.NoError(t, fakeClient.Create(ctx, &token))
+	}
+}
+
+func createBindings(t *testing.T, fakeClient client.Client, bindings ...cerberusv1alpha1.WebserviceAccessBinding) {
+	ctx := context.Background()
+	for _, binding := range bindings {
+		assert.NoError(t, fakeClient.Create(ctx, &binding))
+	}
+}
+
+func createWebservices(t *testing.T, fakeClient client.Client, webservices ...cerberusv1alpha1.WebService) {
+	ctx := context.Background()
+	for _, service := range webservices {
+		assert.NoError(t, fakeClient.Create(ctx, &service))
+	}
+}
+
+func createSecrets(t *testing.T, fakeClient client.Client, secrets ...v1.Secret) {
+	ctx := context.Background()
+	for _, secret := range secrets {
+		assert.NoError(t, fakeClient.Create(ctx, &secret))
+	}
+}
+
+func assertCachesPopulated(t *testing.T, authenticator *Authenticator) {
+	authenticator.cacheLock.RLock()
+	defer authenticator.cacheLock.RUnlock()
+
+	//assert.NotEmpty(t, authenticator.accessCache)
+	assert.NotEmpty(t, authenticator.servicesCache)
 }

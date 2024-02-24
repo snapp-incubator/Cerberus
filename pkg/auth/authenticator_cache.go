fbef72c180578dc09d709d1d7cb0a98ce90ba5ef
Saman Mahdanian <saman@mahdanian.xyz>
2024-02-24 21:38:00 +0330

    emergency: log access cache

--- pkg/auth/authenticator_cache.go
+++ pkg/auth/authenticator_cache.go
@@ -142,7 +142,7 @@ func (a *Authenticator) buildNewWebservicesCache(
 		}
 	}
 
-	a.logger.Info("webservice access cache built successfully", "len", len(newWebservicesCache))
+	a.logger.Info("webservice access cache built successfully", "len", len(newWebservicesCache), "content", newWebservicesCache)
 	return &newWebservicesCache
 }
 
@@ -199,7 +199,7 @@ func (a *Authenticator) buildNewAccessTokensCache(
 		}
 	}
 
-	a.logger.Info("access token cache built successfully", "len", len(newAccessTokensCache))
+	a.logger.Info("access token cache built successfully", "len", len(newAccessTokensCache), "content", newAccessTokensCache)
 	return &newAccessTokensCache
 }
 

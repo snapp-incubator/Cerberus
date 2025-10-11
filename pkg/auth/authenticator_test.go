192e86722e78650944dbc28add58745357edc75e
matehsan <matari.ehsan@yahoo.com>
2025-10-11 16:44:33 +0330

    undo formatting

--- pkg/auth/authenticator_test.go
+++ pkg/auth/authenticator_test.go
@@ -11,14 +11,13 @@ import (
 	"testing"
 	"time"
 
+	cerberusv1alpha1 "github.com/snapp-incubator/Cerberus/api/v1alpha1"
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
-
-	cerberusv1alpha1 "github.com/snapp-incubator/Cerberus/api/v1alpha1"
 )
 
 var (
